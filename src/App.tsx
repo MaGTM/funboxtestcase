88de9894755614e6a8db04794aa7432682c3ec3c
MaGTM <uporoff.maxim@yandex.ru>
2023-01-17 18:54:32 +0600

    main layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import './App.css';
 import Card from "./components/Card/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <Card />
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Ты сегодня покормил кота?</h1>
+            <div className={'Cards'}>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
